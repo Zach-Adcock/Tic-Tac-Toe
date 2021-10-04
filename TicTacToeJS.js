3c7dff57f1dd78beefa2075e4bb5275c9dd3d20d
Zach <daespnman@gmail.com>
2021-10-03 19:38:44 -0700

    functioning tic tac toe game for one round

--- TicTacToeJS.js
+++ TicTacToeJS.js
@@ -3,19 +3,44 @@
 const displayController = ( () => {
 
     //Setting selector indicators
-    const mainDiv = document.getElementsByClassName('main');
+    const mainDiv = document.querySelector('.main');
     const setupContainer = document.querySelector('.setup-container');
+    const gameContainer = document.querySelector('.game-container');
     const gameTitle = document.querySelector('.game-title');
     const playerOneTitle = document.querySelector('.player-one-title');
     const playerTwoTitle = document.querySelector('.player-two-title');
     const playerOneSelector = document.querySelector('#player-one-human-selector');
     const playerTwoSelector = document.querySelector('#player-two-human-selector');
-    // const AIOneSelector = document.querySelector('#player-one-AI-selector');
     const AITwoSelector = document.querySelector('#player-two-AI-selector');
-    
+    const newGameBtn = document.querySelector('.new-game');
+
+
+    const activeButton = e => {
+        const clicked = e.target;
+        if (clicked.classList.contains('active')) return // do nothing if active button is clicked
+        clicked.classList.add('active');
+
+        if (clicked.id == 'player-two-human-selector'){
+            document.querySelector('#player-two-AI-selector').classList.remove('active');
+        } else {
+            document.querySelector('#player-two-human-selector').classList.remove('active');
+        }
+        gamePlay.newGame();
+    }
+
+    playerTwoSelector.addEventListener('click', activeButton);
+    AITwoSelector.addEventListener('click', activeButton);
+
+    //Sets up 'New Game' button to reset board and start new game
+    const startNewGame = () => {
+        gamePlay.newGame()
+    };
+    newGameBtn.addEventListener('click', startNewGame)
+
+
     return {
-        mainDiv, setupContainer, gameTitle, playerOneTitle, playerTwoTitle,
-        playerOneSelector, playerOneTitle, AITwoSelector
+        mainDiv, setupContainer, gameTitle, gameContainer, playerOneTitle, playerTwoTitle,
+        playerOneSelector, playerTwoSelector, AITwoSelector, newGameBtn
     }
 })();
 
@@ -32,31 +57,39 @@ const displayController = ( () => {
 //Create gameboard module (square event listeners, check winning combos)
 const gameBoard = ( () => {
     const boardMoves = ['','','','','','','','','',];
-
+    const getBoardMoves = () => {
+        return boardMoves
+    };
 
     const getSelection = (e) => {
         const squareID = e.target.id;
         makeMove(squareID,'o')
     };
 
+
+    //Make boxes clickable and then unclickable when the game is finished
     const gameSquares = document.querySelectorAll('.gamesquare');
-    gameSquares.forEach(square => square.addEventListener('click', function(e){
-        const selected = e.target;
-        if (selected.innerText !== '') return //user can only click on empty squares
-        gamePlay.makeMove(selected.id);
-    }));
+    const clickableSquares = () => {
+        const changeText = e => {
+            const selected = e.target;
+            if (selected.innerText !== '') return //user can only click on empty squares
+            if (gamePlay.getGameOver() === true) return //no action if game is over
+            gamePlay.makeMove(selected.id);
+        };
+        gameSquares.forEach(square => square.addEventListener('click', changeText));
+    };
+    
 
-
-    //Module for updating the board's appearance
-    var count = 0;
+    //Renders board based on updated boardMoves array
     const renderBoard = () => {
+        var count = 0;
         gameSquares.forEach( square => {
             square.innerText = boardMoves[count];
             count ++ ;
         });
     };
 
-    //Updates array containing the board's marks
+    //Updates array (boardMoves) containing the board's marks
     const updateBoardMoves = () => {
         boardMoves.length = 0; //empty array before updating
         gameSquares.forEach(square => {
@@ -66,53 +99,65 @@ const gameBoard = ( () => {
     }
 
     const clearBoard = () => {
-        boardMoves.fill('');
+        boardMoves.fill(''); //remove marks from board array
+        renderBoard(); 
     }
 
     return {
-        boardMoves,
+        getBoardMoves,
         getSelection,
         renderBoard,
         updateBoardMoves,
-        clearBoard
+        clearBoard,
+        clickableSquares,
+        gameSquares
     };
 
 })();
 
 //Module to track gameplay (create 2 players, control game flow, track scores)
 const gamePlay = (() => {
-    const playerOne = Player('Insert Name 1', 'X');
-    const playerTwo = Player('Insert Name 2', 'O');
+    const playerOne = Player('Human Name 1', 'X');
+    const playerTwo = Player('Human Name 2', 'O');
     const AI = Player('Computer', 'O');
-    let currentPlayer = playerOne;
+    let firstPlayer = playerOne;
     let opposingPlayer = playerTwo;
+    let currentPlayer = playerOne;
     let gameOver = false;
+    let winningMark = '';
     
     //Starts the game when new game is clicked
     const newGame = () => {
-        opposingPlayer = (displayController.playerTwoSelector.classList.contains('active')) ? playerTwo : AI;
-        gameBoard.clearBoard()
+        opposingPlayer = ((displayController.playerTwoSelector).classList.contains('active')) ? playerTwo : AI;
+        gameBoard.clearBoard(); //Start with fresh board
+        // gameMode(); //Sets pvp or pvc (((Don't need this here??)))
     }
-
 
     //Sets gamemode to human v human or human v computer
     const gameMode = () => {
-        //check to see which player buttons are active
-        return 'pvp'
+        if (opposingPlayer === playerTwo) return 'pvp'
+        return 'pvc'
     }
-    // const currentMark = playerOne.getMark;
 
+    // const currentMark = playerOne.getMark;
+    const gameCheck = () => {
+        console.log('currentPlayer: '  + currentPlayer.getName() + currentPlayer.getMark());  
+        console.log('firstPlayer: '  + firstPlayer.getName() + firstPlayer.getMark());  
+        console.log('opposingPlayer: '  + opposingPlayer.getName() + opposingPlayer.getMark());  
+        console.log('gameOver: '  + gameOver);  
+        console.log('gameMode: '  + gameMode());  
+    }
 
 
     //Updates DOM based on player's move
     const makeMove = (squareID) => {
-        const checkID = parseInt(squareID[squareID.length-1])-1;
+        const checkID = parseInt(squareID[squareID.length-1])-1;  //determines the index of the square using its ID
         if (checkID < 0 || checkID > 8) return
         if (currentPlayer.getMark() == 'X' || currentPlayer.getMark() == 'O') {
             document.getElementById(`${squareID}`).innerText = currentPlayer.getMark();
-            gameBoard.updateBoardMoves()
-            if (winningMove() === true) {
-                console.log('Game Over');
+            gameBoard.updateBoardMoves();
+            winningMove()
+            if (getGameOver() === true || getGameOver() === 'tie') {
                 endGame();
             };
             togglePlayer();
@@ -120,14 +165,11 @@ const gamePlay = (() => {
     };
     const togglePlayer = () => {
         if (gameMode() === 'pvp'){
-            console.log(currentPlayer.getName())
-
             currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
-            console.log(currentPlayer.getMark())
         } else if (gameMode() === 'pva'){
+            currentPlayer = (currentPlayer === playerOne) ? AI : playerOne;
         }
         };
-
 
     //Check to see if game is over
     const winningMove = () => {
@@ -141,35 +183,57 @@ const gamePlay = (() => {
             [0,4,8],
             [2,4,6],
         ];
-        let j = gameBoard.boardMoves;
-        // console.log(j[winVariations[0][2]]);
+        let j = gameBoard.getBoardMoves();
         for (let i = 0; i<winVariations.length; i++){
-            if (j[winVariations[i][0]] == j[winVariations[i][1]] && j[winVariations[i][0]] == j[winVariations[i][2]]){
-                console.log(j[winVariations[i][0]] + j[winVariations[i][1]] + j[winVariations[i][2]])
-                gameOver = (j[winVariations[i][0]] == 'X' || j[winVariations[i][0]] == 'O') ? true : false; 
-                return gameOver
-            }
-        };       
+            if ((j[winVariations[i][0]] == j[winVariations[i][1]]) && (j[winVariations[i][0]] == j[winVariations[i][2]])
+            && (j[winVariations[i][0]] == 'X' || j[winVariations[i][0]] == 'O')){
+                winningMark = j[winVariations[i][0]];
+                gameOver = true;
+                gameBoard.clickableSquares(); 
+            }; 
+        }; 
+        if ((j.every(square => square !== '')) && (gameOver !== true)) {
+            gameOver = 'tie';
+        }      
+    }
+
+    //getter for the live value of gameOver
+    const getGameOver = () => {
+        console.log('gameOver = ' + gameOver);
+        return gameOver
     }
 
     const endGame = () => {
-
+        const endMessage = document.createElement('div');
+        const winner = (winningMark == 'X')? firstPlayer : opposingPlayer;
+        if (gameOver == true) {
+            endMessage.innerText = `${winner.getName()} is the winner.`;
+        } else {
+            endMessage.innerText = `This round is a tie.`;
+        };
+        let parentNode = displayController.mainDiv;
+        let childNode = displayController.gameContainer;
+        console.log(typeof parentNode);
+        parentNode.insertBefore(endMessage, childNode);
     }
+
 
     return {
         makeMove,
         togglePlayer,
         winningMove,
-        endGame
+        endGame,
+        newGame,
+        gameCheck,
+        getGameOver
     }
 
 })();
 
+gameBoard.clickableSquares() //needs initialization after gamePlay module is created
 
 
 
 
-gameBoard.renderBoard()
 
 
-//Add event listeners to squares
